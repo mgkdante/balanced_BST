d4c323ef65b612ad6d574c8978e86ffff41c135d
yesid <mgkdante@pm.me>
2024-06-12 17:16:39 -0400

    Added test by TOP

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
 import Tree from "./tree.js"
 
-const longArray = [
-  38, 15, 27, 94, 7, 53, 89, 2, 68, 34, 72, 14, 56, 3, 81, 42, 10, 76, 23, 60,
-  99, 5, 31, 87, 26, 49, 12, 73, 19, 91, 8, 45, 66, 22, 59, 33, 85, 11, 40, 64,
-]
-const tree = new Tree(longArray)
+const longArray = (max = 100) => {
+  let array = []
+  for (let i = 0; i < 100; i++) {
+    array.push(Math.floor(Math.random() * max))
+  }
+  return array
+}
+
+const tree = new Tree(longArray())
 tree.prettyPrint()
+console.log(tree.isBalanced())
+console.log(tree.preOrder())
 console.log(tree.postOrder())
+console.log(tree.inOrder())
+const multipleInsert = (max = 100) => {
+  for (let i = 0; i < 100; i++) {
+    tree.insert(Math.floor(Math.random() * max))
+  }
+}
+multipleInsert()
+tree.prettyPrint()
+console.log(tree.isBalanced())
+tree.rebalance()
+console.log(tree.isBalanced())
+console.log(tree.preOrder())
+console.log(tree.postOrder())
+console.log(tree.inOrder())
+tree.prettyPrint()
